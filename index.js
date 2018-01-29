d5d38e9b6b27ff446e428935a1f7c3d6d41d3e8e
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-29 23:33:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function findMatching(collection, query) {
  return collection.filter(function (name) {
-   return name.toLowerCase() === query.toLowerCase() 
+   return name.toLowerCase() === query.toLowerCase();
    })
+}
+
+function fuzzyMatch(collection, query) {
+  return collection.filter(function (letter) {
+    return letter === query[0] 
+  }) 
 }
