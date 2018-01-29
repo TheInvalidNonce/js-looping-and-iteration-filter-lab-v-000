2e75108178f990e2277c3ef1e77fcc8b07ea71ab
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-29 23:56:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function findMatching(collection, query) {
- return collection.filter(function (name) {
-   return name.toLowerCase() === query.toLowerCase();
+ return collection.filter(function (el) {
+   return el.toLowerCase() === query.toLowerCase();
    })
 }
 
 function fuzzyMatch(collection, query) {
-  return collection.filter(function (letter) {
-    return letter[0] === query[0]
+  return collection.filter(function (el) {
+    return el[0] === query[0]
   }) 
 }
 
 function matchName(collection, query) {
-  return collection.filter(function (collection) {
-     return collection['name'] === query 
+  return collection.filter(function (el) {
+     return el['name'] === query 
   })
 }
